c5dd5bb26bb7fdcce0bd1bee1778fe103218f0b7
kamranjune <khsd6920@gmail.com>
2019-01-11 15:47:06 -0800

    port = process.env.PORT || 3000;

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 //models/index.js
 //var PORT = 3000;
+PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
